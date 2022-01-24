3636fa36ad801f72c917153deaec7295cfab5246
amberitas17 <67531380+amberitas17@users.noreply.github.com>
2022-01-24 15:55:43 +0800

    deleting things

--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import Hello from './Hello';
+import 'tachyons';
 import { register as registerServiceWorker } from './registerServiceWorker';
-ReactDOM.render(<Hello />, document.getElementById('root'));
+
+ReactDOM.render(<Hello greeting={'Hello' + 'React Ninja'} />, document.getElementById('root'));
 registerServiceWorker();
