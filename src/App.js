e7da8c4a9a2c680662bf07932f7eb8734d0934ed
amberitas17 <67531380+amberitas17@users.noreply.github.com>
2022-01-25 10:50:40 +0800

    json fetch

--- src/App.js
+++ src/App.js
@@ -1,18 +1,23 @@
 import React, { Component } from 'react';
 import CardList from './CardList';
 import SearchBox from './SearchBox'
-import { robots } from './robots';
 import './App.css';
 
 class App extends Component {
     constructor(){
         super()
         this.state = {
-            robots: robots,
+            robots: [],
             searchfield: ''
         }
     }
 
+    componentDidMount() {
+        fetch('https://jsonplaceholder.typicode.com/users')
+        .then(response=> response.json())
+        .then(users => {this.setState({ robots: users })});
+    }
+ 
     onSearchChange = (event) => {
         this.setState({ searchfield: event.target.value })
     }
@@ -21,6 +26,9 @@ class App extends Component {
         const filteredRobots = this.state.robots.filter(robots => {
             return robots.name.toLowerCase().includes(this.state.searchfield.toLowerCase());
         })
+        if (this.state.robots.length === 0){
+            return <h1>Loading</h1>
+        } else {
         return (
             <div className='tc'>
                 <h1 className='f1'>RoboFriends</h1>
@@ -28,6 +36,7 @@ class App extends Component {
                 <CardList robots={filteredRobots} />
             </div>
         );
+        }
     }
 }
 
