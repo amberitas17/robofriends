205eefda1e920d6a3b4b4ef7048fc8e9a681fce1
amberitas17 <67531380+amberitas17@users.noreply.github.com>
2022-01-25 15:12:39 +0800

    improving the code through hooks

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,31 +1,35 @@
-import React, { Component } from 'react';
+import React, { useState, useEffect } from 'react';
 import CardList from '../components/CardList';
 import SearchBox from '../components/SearchBox'
 import './App.css';
 import Scroll from '../components/Scroll';
-import ErrorBoundary from '../components/ErrorBoundary';
 
-class App extends Component {
-    constructor(){
+function App () {
+    /* constructor(){
         super()
         this.state = {
             robots: [],
             searchfield: ''
-        }
-    }
+        } 
+    }*/
+    const [robots, setRobots] = useState([])
+    const [searchfield, setSearchfield] = useState('')
 
-    componentDidMount() {
+    /* componentDidMount() {
         fetch('https://jsonplaceholder.typicode.com/users')
         .then(response=> response.json())
         .then(users => {this.setState({ robots: users })});
-    }
- 
-    onSearchChange = (event) => {
-        this.setState({ searchfield: event.target.value })
-    }
+    } */
 
-    render(){
-        const { robots, searchfield } = this.state
+    useEffect(()=> {
+      fetch('https://jsonplaceholder.typicode.com/users')
+        .then(response=> response.json())
+        .then(users => {setRobots(users)});
+    },[]) 
+ 
+    const onSearchChange = (event) => {
+      setSearchfield(event.target.value)
+    }
         const filteredRobots = robots.filter(robot => {
             return robot.name.toLowerCase().includes(searchfield.toLowerCase());
         })
@@ -34,15 +38,13 @@ class App extends Component {
       (
         <div className='tc'>
           <h1 className='f1'>RoboFriends</h1>
-          <SearchBox searchChange={this.onSearchChange}/>
+          <SearchBox searchChange={onSearchChange}/>
           <Scroll>
-              <ErrorBoundary>
                 <CardList robots={filteredRobots} />
-              </ErrorBoundary>
           </Scroll>
         </div>
       );
-    }
+
 }
 
 export default App;
